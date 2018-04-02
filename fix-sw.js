73878eeb148afb5a201a2dea084ea1cba58654f0
Aditya Ammanabrolu <ammanabrolu.aditya@gmail.com>
2018-04-02 14:04:32 +0530

    init changes

--- fix-sw.js
+++ fix-sw.js
@@ -73,3 +73,11 @@ try {
 catch (error) {
     console.error('Error occurred while looking for baseHref: ', error);
 }
+const express = require('express');
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
